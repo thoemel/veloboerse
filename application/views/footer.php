bced05fe8b43a53152beb8ef91d61f472113a8ab
thoemel <thoemel@thoemel.ch>
2013-12-27 20:08:00 +0100

    google analytics rausgeschmissen

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,12 +1,10 @@
-<?php 
-echo '
-		<footer>
-		' . anchor('', img('img/logo_nur_ringe.png'), array('title'=>'Start'));
-
-?>
-		</footer>
 
         </div> <!-- /container -->
+		
+		<footer>
+<?php echo anchor('', img('img/logo_nur_ringe.png'), array('title'=>'Start')); ?>
+		</footer>
+
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
@@ -14,13 +12,6 @@ echo '
         <script src="<?php echo base_url();?>js/vendor/bootstrap.min.js"></script>
 
         <script src="<?php echo base_url();?>js/main.js"></script>
-
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
 
 </body>
 </html>

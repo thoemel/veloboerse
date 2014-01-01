6b6fd1e791ce073d813226f03bad6a6926a3780a
thoemel <thoemel@thoemel.ch>
2014-01-01 20:58:10 +0100

    fixes #34

--- application/views/haendleradmin/auswahl.php
+++ application/views/haendleradmin/auswahl.php
@@ -10,9 +10,8 @@ echo '
 			<tr>
 				<th scope="column">Firma</th>
 				<th scope="column">Person</th>
-				<th scope="column">Velos</th>
-				<th scope="column">Import</th>
 				<th scope="column">Quittungen</th>
+				<th scope="column">Abrechnung</th>
 			</tr>
 		</thead>
 		<tbody>';
@@ -22,9 +21,8 @@ foreach ($liste->result() as $haendler) {
 			<tr>
 				<th scope="row">' . $haendler->firma . '</th>
 				<td>' . $haendler->person . '</td>
-				<td>' . anchor('haendleradmin/velos/' . $haendler->id, 'Velos'). '</td>
-				<td>' . anchor('haendleradmin/import/' . $haendler->id, 'CSV Import') . '</td>
 				<td>' . anchor('haendleradmin/quittungen/' . $haendler->id, 'Quittungen zuweisen', 'inactive="true"') . '</td>
+				<td>' . anchor('auszahlung/velos/' . $haendler->id, 'Abrechnung'). '</td>
 			</tr>';
 }	
 

90c3dbdd113eba3c7b3261d58dac3bea4febd409
thoemel <thoemel@thoemel.ch>
2015-05-15 13:20:22 +0200

    #73 Noch den View zum Verklickt...

---
+++ application/views/kasse/verklickt.php
@@ -0,0 +1,34 @@
+<?php 
+include APPPATH . 'views/header.php';
+
+echo '
+
+<div>
+	<h1>Verklickt</h1>
+	
+	<div class="alert alert-error">
+		Du hast das vorherige Velo nicht abgeschlossen! 
+	</div>
+	<div class="row">
+		<div class="col-sm-4">
+			Quittung Nr. des nicht abgeschlossenen Velos:
+		</div>
+		<div class="badge col-sm-2">
+			' . $quittungNr . '
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			Du musst die Quittungsnummer noch einmal oben rechts eingeben und den 
+			Verkauf abschliessen! 
+		</div>
+	</div>
+	
+</div>
+			
+<div>
+	<p><br><br><br><br><br><br>
+		' . anchor('kasse/', 'Dieses Velo nicht verkaufen.') . '</p>
+</div>';
+
+include APPPATH . 'views/footer.php';

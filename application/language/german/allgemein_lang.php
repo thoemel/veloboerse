940c1d547e4ed51c1ec42650a679d6705cf9a8bf
thoemel <thoemel@thoemel.ch>
2020-09-03 13:37:35 +0200

    Fehlende form_validation Texte ergänzt

--- application/language/german/allgemein_lang.php
+++ application/language/german/allgemein_lang.php
@@ -2,3 +2,5 @@
 $lang['yes']			= "ja";
 $lang['no']				= "nein";
 $lang['form_validation_future_date'] = 'Das Datum muss in der Zukunft sein.';
+$lang['form_validation_check_unique_username'] = 'Dieser Benutzername ist schon vergeben.';
+$lang['form_validation_matches'] = 'Die eingegebenen Passwörter stimmen nicht überein.';

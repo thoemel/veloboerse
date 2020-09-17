cde45082eff0f82c731eb3b75ebfb719eb10c5d8
thoemel <thoemel@thoemel.ch>
2020-09-17 15:42:28 +0200

    #110 Velafrika zuoberst im Veloformular

--- application/views/velos/formular.php
+++ application/views/velos/formular.php
@@ -71,6 +71,14 @@ echo '
 	</div>
 
 	<div class="form-group">
+		<label class="col-sm-2 control-label">Velafrika</label>
+		<div class="col-sm-10">
+			<label class="radio-inline">' . form_radio('velafrika', 1, (true == $myVelo->afrika)) . ' ja</label>
+			<label class="radio-inline">' . form_radio('velafrika', 0, (false == $myVelo->afrika)) . ' nein</label>
+		</div>
+	</div>
+
+	<div class="form-group">
 		<label for="typ_input" class="col-sm-2 control-label">Typ</label>
 		<div class="col-sm-4">
 			' . form_input(array('id' => 'typ_input', 'name' => 'typ', 'value' => $myVelo->typ, 'class' => 'form-control')) . '
@@ -175,13 +183,6 @@ echo '
 		<div class="col-sm-10">
 			<label class="radio-inline">' . form_radio('storniert', 1, (true == $myVelo->storniert)) . ' ja</label>
 			<label class="radio-inline">' . form_radio('storniert', 0, (false == $myVelo->storniert)) . ' nein</label>
-		</div>
-	</div>
-	<div class="form-group">
-		<label class="col-sm-2 control-label">Velafrika</label>
-		<div class="col-sm-10">
-			<label class="radio-inline">' . form_radio('velafrika', 1, (true == $myVelo->afrika)) . ' ja</label>
-			<label class="radio-inline">' . form_radio('velafrika', 0, (false == $myVelo->afrika)) . ' nein</label>
 		</div>
 	</div>
 
